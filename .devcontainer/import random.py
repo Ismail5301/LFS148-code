c69452b3104b3ff09ef53cb27a28546496ca16ba
ismail5301 <ismail530@gmail.com>
2026-09-08 17:13:19 -0400

    Java instrumentation annotations for OTEL

---
+++ .devcontainer/import random.py
@@ -0,0 +1,5 @@
+import random
+
+random_number = random.randint(1, 10)
+
+print ("Your random number is:", random_number)
--- exercises/automatic-instrumentation/initial/todobackend-springboot/src/main/java/io/novatec/todobackend/TodobackendApplication.java
+++ exercises/automatic-instrumentation/initial/todobackend-springboot/src/main/java/io/novatec/todobackend/TodobackendApplication.java
@@ -13,6 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.*;
 
+import io.opentelemetry.instrumentation.annotations.SpanAttribute;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -77,8 +80,8 @@ public class TodobackendApplication {
 		return todo;
 
 	}
-
-	String someInternalMethod(String todo){
+	@WithSpan String
+	someInternalMethod(@SpanAttribute String todo){
 
 		todoRepository.save(new Todo(todo));
 		if(todo.equals("slow")){
